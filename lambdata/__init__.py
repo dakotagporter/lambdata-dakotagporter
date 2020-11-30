1c71e145c6d61d715d162b2444e541d632dbd616
Dakota Porter <dakotap3045@gmail.com>
2020-11-30 11:46:26 -0700

    Creating Package

---
+++ lambdata/__init__.py
@@ -0,0 +1,9 @@
+"""Lambdata - a collection of Data Science helper functions
+    Any methods in this file are attached directly to 'lambdata' import"""
+
+import pandas as pd
+import numpy as np
+
+def df_cleaner(df):
+    """Cleans a DataFrame with null values"""
+    # TODO - implement

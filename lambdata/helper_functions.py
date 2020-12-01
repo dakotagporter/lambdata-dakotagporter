88491a54bee6ec62963745e4cc9e57d3acad01fa
Dakota Porter <72000859+dakotagporter@users.noreply.github.com>
2020-12-01 14:14:57 -0700

    Add files via upload

--- lambdata/helper_functions.py
+++ lambdata/helper_functions.py
@@ -6,13 +6,40 @@ from sklearn.model_selection import train_test_split
 
 
 def null_count(df):
-
+    """
+    Returns total amount of null values in the DataFrame.
+    """
     nulls = df.isnull().sum()
 
     return nulls
 
-def train_test_split_func(df, frac):
 
-    train, test = train_test_split(df, train_size=frac, random_state=42)
+def train_test_split_func(df, frac):
+    """
+    Does a train-test-split using sklearn.
+    """
+    train, test = train_test_split(
+                    df, train_size=frac, test_size=(1-frac), random_state=42)
 
     return train, test
+
+
+class ColAvg(pd.DataFrame):
+    """
+    Returns average of each int columns in a given DataFrame.
+    """
+    def calculate(self):
+        avgs = []
+
+        for col in self.columns:
+            if self[col].dtype == 'int64':
+                avg = self[col].sum() / len(self[col])
+                avgs.append(avg)
+            else:
+                avgs.append(None)
+
+        return avgs
+
+
+if __name__ == "__main__":
+    pass

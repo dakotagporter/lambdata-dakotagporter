5b505aa6f4a21451ef794359b390452bfaeabe75
dakotagporter <dakotap3045@gmail.com>
2020-12-03 15:20:33 -0700

    unittests

--- lambdata/test_lambdata.py
+++ lambdata/test_lambdata.py
@@ -9,9 +9,13 @@ import helper_functions
 class ExampleTests(unittest.TestCase):
     def test_nullcount(self):
         """
-        Testing that nullcount function works as expected...
+        Testing that null_count function works as expected
         """
         df = pd.DataFrame({"A": [1, np.NaN, 3],
                            "B": ['a', 'b', np.NaN],
                            "C": [4, 5, 6]})
-        self.assertIsInstance(helper_functions.null_count(df), int)
+        self.assertEqual(helper_functions.null_count(df).dtype, 'int64')
+
+
+if __name__ == '__main__':
+    unittest.main()
